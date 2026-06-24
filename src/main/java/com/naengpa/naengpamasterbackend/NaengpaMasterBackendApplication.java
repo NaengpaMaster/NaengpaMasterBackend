8b70236aad0a7afb5530e2c0eede482f27b883c2
HaewonJeong <bonjour_haewon@naver.com>
2026-06-24 14:45:42 +0900

    초기 세팅

---
+++ src/main/java/com/naengpa/naengpamasterbackend/NaengpaMasterBackendApplication.java
@@ -0,0 +1,13 @@
+package com.naengpa.naengpamasterbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NaengpaMasterBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NaengpaMasterBackendApplication.class, args);
+    }
+
+}
