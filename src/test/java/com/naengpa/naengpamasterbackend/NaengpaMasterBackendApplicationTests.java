cc752b80f1e52f5c3af79a696fdd5cb147745a0e
HaewonJeong <bonjour_haewon@naver.com>
2026-06-24 14:45:42 +0900

    초기 세팅

---
+++ src/test/java/com/naengpa/naengpamasterbackend/NaengpaMasterBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.naengpa.naengpamasterbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NaengpaMasterBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
