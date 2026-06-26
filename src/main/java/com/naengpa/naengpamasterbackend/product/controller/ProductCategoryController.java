266fbb25804644eec61e354266bba21fa82751a9
nmhyrhn <gbs06193@gmail.com>
2026-06-27 01:44:33 +0900

    feat: 카테고리 목록 조회 API 구현 및 테스트 추가

--- src/main/java/com/naengpa/naengpamasterbackend/product/controller/ProductCategoryController.java
+++ src/main/java/com/naengpa/naengpamasterbackend/product/controller/ProductCategoryController.java
@@ -1,10 +1,7 @@
 package com.naengpa.naengpamasterbackend.product.controller;
 
 import com.naengpa.naengpamasterbackend.product.dto.response.ProductCategoryResponse;
-import com.naengpa.naengpamasterbackend.product.entity.ProductCategory;
-import com.naengpa.naengpamasterbackend.product.repository.ProductCategoryRepository;
 import com.naengpa.naengpamasterbackend.product.service.ProductCategoryService;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -15,8 +12,7 @@ import java.util.List;
 @RequestMapping("/api/v1/categories")
 public class ProductCategoryController {
 
-    @Autowired
-    ProductCategoryService productCategoryService;
+    private final ProductCategoryService productCategoryService;
 
     public ProductCategoryController(ProductCategoryService productCategoryService) {
         this.productCategoryService = productCategoryService;
--- src/main/java/com/naengpa/naengpamasterbackend/product/repository/ProductCategoryRepository.java
+++ src/main/java/com/naengpa/naengpamasterbackend/product/repository/ProductCategoryRepository.java
@@ -3,8 +3,6 @@ package com.naengpa.naengpamasterbackend.product.repository;
 import com.naengpa.naengpamasterbackend.product.entity.ProductCategory;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-import java.util.List;
-
 public interface ProductCategoryRepository extends JpaRepository<ProductCategory, Long> {
 
 }
