8b111952caf6b4655921c13eca62da8c216e18b1
namchaerin <cr434972@gmail.com>
2026-07-03 13:11:57 +0900

    feat: 레시피 수정 음식카테고리 필수화 및 상세 응답에 음식카테고리 추가

--- src/main/java/com/naengpa/naengpamasterbackend/recipe/repository/RecipeFoodCategoryRepository.java
+++ src/main/java/com/naengpa/naengpamasterbackend/recipe/repository/RecipeFoodCategoryRepository.java
@@ -7,8 +7,11 @@ import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
 import java.util.List;
+import java.util.Optional;
 
 public interface RecipeFoodCategoryRepository extends JpaRepository<RecipeFoodCategory, Long> {
+
+    Optional<RecipeFoodCategory> findByRecipeId(Long recipeId);
 
     List<RecipeFoodCategory> findByRecipeIdIn(List<Long> recipeIds);
 
