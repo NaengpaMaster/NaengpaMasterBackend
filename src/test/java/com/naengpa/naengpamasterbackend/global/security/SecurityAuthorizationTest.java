ae1ed8506e0026db0b01fedadb3c371595496485
김혜령 <he2089@naver.com>
2026-07-07 10:07:12 +0900

    fix:qa 오류사항 수정

--- src/test/java/com/naengpa/naengpamasterbackend/global/security/SecurityAuthorizationTest.java
+++ src/test/java/com/naengpa/naengpamasterbackend/global/security/SecurityAuthorizationTest.java
@@ -19,6 +19,7 @@ import org.springframework.http.MediaType;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.time.LocalDateTime;
 
@@ -32,6 +33,7 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 
 @SpringBootTest
 @AutoConfigureMockMvc
+@Transactional
 class SecurityAuthorizationTest {
 
     @Autowired
